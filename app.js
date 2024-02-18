2558a58224fc36c7c6b2c018b6f079849de23d97
Arja-Balu <111408472+Arja-Balu@users.noreply.github.com>
2024-02-18 15:55:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,364 +1,310 @@
-const express = require('express')
-const sports = express()
-const bodyParser = require('body-parser')
-var csrf = require("tiny-csrf")
+let hex;
+const express = require('express');
+const sports = express();
+const bodyParser = require('body-parser');
+const csrf = require('tiny-csrf');
+const passport = require('passport');
+const connectEnsureLogin = require('connect-ensure-login');
+const session = require('express-session');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const { Registers, AllEvents, User } = require('./models');
+const cookieParser = require('cookie-parser');
+
+
 sports.use('/images', express.static('images'));
-const passport=require("passport");
-const connectEnsureLogin = require("connect-ensure-login");
-const session =  require("express-session");
-const LocalStrategy = require("passport-local")
-const bcrypt=require("bcrypt")
-const saltRounds = 10
-const {Registers,AllEvents,  User } = require('./models')
-
-var cookieParser = require("cookie-parser")
-sports.use(bodyParser.json())
-sports.set('views', './views');
-const path=require("path");
-const allevents = require('./models/allevents');
-const { register } = require('module');
-sports.use(express.urlencoded({extended:false}));
-sports.use(cookieParser("shh! some secret string"))
-sports.use(csrf("this_should_be_32_character_long",["POST","PUT","DELETE"]))
-
-sports.set("view engine","ejs");
-sports.use(express.static(path.join(__dirname,"public")));
-
+sports.use(bodyParser.json());
+sports.use(bodyParser.urlencoded({ extended: false }));
+sports.use(cookieParser("shh! some secret string"));
+sports.use(csrf("this_should_be_32_character_long",["POST","PUT","DELETE"]));
 sports.use(session({
-  secret:"my_super_secret_key_123456789",
-  cookie:{
-    maxAge:24*60*60*1000 
-  }
-}))
-
-
+  secret: "my_super_secret_key_123456789",
+  cookie: { maxAge: 24 * 60 * 60 * 1000 }
+}));
 sports.use(passport.initialize());
 sports.use(passport.session());
-
-
-const isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next(); 
-  } else {
-    res.redirect('/'); 
-  }
-};
-
+sports.set('views', './views');
+sports.set('view engine', 'ejs');
+sports.use(express.static('public'));
 
 
 passport.use(new LocalStrategy({
-  usernameField:'email',
-  passwordField:'password'
-},(username,password,done)=>{
+  usernameField: 'email',
+  passwordField: 'password'
+}, authenticateUser));
+
+passport.serializeUser((user, done) => {
+  console.log("Serializing user in session", user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then(user => done(null, user))
+    .catch(err => done(err, null));
+});
+
+
+sports.post("/loginSubmit", passport.authenticate('local', { failureRedirect: "/studentLogin" }), loginSubmitHandler);
+
+sports.get("/login", (req, res) => renderLogin(req, res, { title: "Login", csrfToken: req.csrfToken() }));
+
+sports.get("/signup", (req, res) => renderSignup(req, res, { title: "Signup", csrfToken: req.csrfToken() }));
+
+sports.post("/signupSubmit", signupSubmitHandler);
+
+sports.get('/adminDashboard', connectEnsureLogin.ensureLoggedIn(), isAuthenticated, adminDashboardHandler);
+
+sports.get('/addEvent', connectEnsureLogin.ensureLoggedIn(), addEventHandler);
+
+sports.post('/uploadEvent', connectEnsureLogin.ensureLoggedIn(), uploadEventHandler);
+
+sports.get('/allEvents', connectEnsureLogin.ensureLoggedIn(), allEventsHandler);
+
+sports.get("/deleteEvent", connectEnsureLogin.ensureLoggedIn(), deleteEventHandler);
+
+sports.get('/dashBoard', connectEnsureLogin.ensureLoggedIn(), dashBoardHandler);
+
+sports.get('/viewEvent', connectEnsureLogin.ensureLoggedIn(), viewEventHandler);
+
+sports.get("/myEvents", connectEnsureLogin.ensureLoggedIn(), myEventsHandler);
+
+sports.get("/viewRegisters", connectEnsureLogin.ensureLoggedIn(), viewRegistersHandler);
+
+sports.get("/registerEvent", connectEnsureLogin.ensureLoggedIn(), registerEventHandler);
+
+sports.get("/unRegister", connectEnsureLogin.ensureLoggedIn(), unRegisterHandler);
+
+
+function authenticateUser(username, password, done) {
   console.log(username);
   User.findOne({
-    where:{
-      email:username
-    }
-  }).then(async (user)=>{
-    const result = await bcrypt.compare(password,user.password)
-    if(result){
-      return done(null,user);
-    }
-    else{
-      return done("Invalid Password")
-    }
-  }).catch((err)=>{
-    return (err)
+    where: { email: username }
   })
-}))
-
-
-passport.serializeUser((user,done)=>{
-  console.log("Serilalizing user in session",user.id)
-  done(null,user.id)
-})
-
-passport.deserializeUser((id,done)=>{
-  User.findByPk(id)
-  .then(user=>{
-    done(null,user)
-  }).catch(err=>{
-    done(err,null)
-  })
-})
-
-
-
-
-
-
-sports.post("/loginSubmit", passport.authenticate('local',{failureRedirect:"/studentLogin"}), (request, response) => {
-  console.log(request.user.email)
-  if(request.user.email==="balu1234@gmail.com"){
-    response.redirect("/adminDashboard")
-  }
-  else{
-    response.redirect("/dashboard");
-  }
-});
-
-
-sports.get("/login",(request,response)=>{
-    response.render("login",{title:"Login", csrfToken: request.csrfToken()})
-})
-
-
-sports.get("/signup",(request,response)=>{
-    response.render("signup",{title:"Signup", csrfToken: request.csrfToken()})
-})
-
-
-sports.post("/signupSubmit", async (request, response) => {
-  const hashedPwd = await  bcrypt.hash(request.body.password,saltRounds)
-  try{
-    const user=await User.create({
-      firstName: request.body.firstName,
-      lastName: request.body.lastName,
-      email:request.body.email,
-      password:hashedPwd,
+    .then(async (user) => {
+      const result = await bcrypt.compare(password, user.password);
+      if (result) {
+        return done(null, user);
+      } else {
+        return done("Invalid Password");
+      }
     })
-    request.login(user,(err)=>{
-       if(err){
-          console.log(err);
-       }
-       response.redirect("/login")
-    })
+    .catch((err) => done(err));
+}
+
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/');
   }
-  catch(err){
-    console.log(err)
+}
+
+
+function loginSubmitHandler(req, res) {
+  console.log(req.user.email);
+  if (req.user.email === "balu1234@gmail.com") {
+    res.redirect("/adminDashboard");
+  } else {
+    res.redirect("/dashboard");
   }
-})
+}
 
+function renderLogin(req, res, data) {
+  res.render("login", data);
+}
 
-sports.get('/adminDashboard',  connectEnsureLogin.ensureLoggedIn(),isAuthenticated, (request, response) => {
-  console.log(request.user)
-  response.render('adminDashboard', {name: request.user.firstName}); 
-});
+function renderSignup(req, res, data) {
+  res.render("signup", data);
+}
 
-
-sports.get('/addEvent', connectEnsureLogin.ensureLoggedIn(), (request, response) => {
-  response.render('addEvent', { title: 'Signup', csrfToken: request.csrfToken() });
-});
-
-
-sports.post('/uploadEvent', connectEnsureLogin.ensureLoggedIn(), async (request, response) => {
+async function signupSubmitHandler(req, res) {
+  const hashedPwd = await bcrypt.hash(req.body.password, saltRounds);
   try {
-  
-
-    await AllEvents.create({
-      eventUserId: request.user.id, 
-      eventImg: request.body.EventImg,
-      eventTitle: request.body.EventTitle,
-      eventDesc: request.body.content,
-      eventVenue: request.body.EventLocation,
-      eventCapacity: request.body.EventMemebers,
-      eventStartDate: request.body.EventStartDate,
-      eventTime: request.body.EventTime,
-      eventEndDate: request.body.EventEndDate,
+    const user = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hashedPwd,
     });
-
-    response.redirect('/allEvents');
+    req.login(user, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      res.redirect("/login");
+    });
   } catch (err) {
     console.log(err);
-    
-    response.status(500).send('Internal Server Error');
   }
-});
+}
 
-sports.get('/allEvents', connectEnsureLogin.ensureLoggedIn(), async (request, response) => {
-  
+function adminDashboardHandler(req, res) {
+  console.log(req.user);
+  res.render('adminDashboard', { name: req.user.firstName });
+}
+
+function addEventHandler(req, res) {
+  res.render('addEvent', { title: 'Signup', csrfToken: req.csrfToken() });
+}
+
+async function uploadEventHandler(req, res) {
+  try {
+    await AllEvents.create({
+      eventUserId: req.user.id,
+      eventImg: req.body.EventImg,
+      eventTitle: req.body.EventTitle,
+      eventDesc: req.body.content,
+      eventVenue: req.body.EventLocation,
+      eventCapacity: req.body.EventMemebers,
+      eventStartDate: req.body.EventStartDate,
+      eventTime: req.body.EventTime,
+      eventEndDate: req.body.EventEndDate,
+    });
+    res.redirect('/allEvents');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Internal Server Error');
+  }
+}
+
+async function allEventsHandler(req, res) {
   try {
     const EventData = await AllEvents.findAll();
     const FormattedEventData = EventData.map(EventData => ({
-    id: EventData.id,
-    UserId: EventData.eventUserId,
-    eventImg: EventData.eventImg,
-    eventTitle: EventData.eventTitle,
-    eventDesc: EventData.eventDesc,
-    eventVenue: EventData.eventVenue,
-    eventCapacity: EventData.eventCapacity,
-    eventStartDate: EventData.eventStartDate,
-    eventTime: EventData.eventTime,
-    eventEndDate: EventData.eventEndDate,
-    createdAt: EventData.createdAt,
-    updatedAt: EventData.updatedAt,
+      id: EventData.id,
+      UserId: EventData.eventUserId,
+      eventImg: EventData.eventImg,
+      eventTitle: EventData.eventTitle,
+      eventDesc: EventData.eventDesc,
+      eventVenue: EventData.eventVenue,
+      eventCapacity: EventData.eventCapacity,
+      eventStartDate: EventData.eventStartDate,
+      eventTime: EventData.eventTime,
+      eventEndDate: EventData.eventEndDate,
+      createdAt: EventData.createdAt,
+      updatedAt: EventData.updatedAt,
     }));
-    if(request.user.email==="balu1234@gmail.com"){
-      response.render('allEvents', { title: 'AllEvents',name: request.user.firstName ,FormattedEventData,  csrfToken: request.csrfToken() });
-    }
-    else{
-      console.log("hello")
-      hex=0;
-      response.render('SallEvents', { title: 'AllEvents',name: request.user.firstName ,FormattedEventData,  csrfToken: request.csrfToken() });
+    if (req.user.email === "balu1234@gmail.com") {
+      res.render('allEvents', { title: 'AllEvents', name: req.user.firstName, FormattedEventData, csrfToken: req.csrfToken() });
+    } else {
+      console.log("hello");
+      hex = 0;
+      res.render('SallEvents', { title: 'AllEvents', name: req.user.firstName, FormattedEventData, csrfToken: req.csrfToken() });
     }
   } catch (error) {
     console.error('Error fetching todos:', error);
-    return response.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-  
-});
-
-
-
-
-sports.get("/deleteEvent", connectEnsureLogin.ensureLoggedIn(),async (request,response)=>{
-  const eventId=request.query.eventId;
-  console.log("ggdghfghfhfhF"+eventId)
-  try{
-  await AllEvents.destroy({
-    where:{
-      id:eventId
-    }
-  })
-  response.redirect('/allEvents')
 }
-catch(err){
-  console.log(err)
-}
-})
 
-
-// ------------- USER FUNTIONALITIES -------------
-
-let hex=0
-
-
-sports.get('/dashBoard',  connectEnsureLogin.ensureLoggedIn(), (request, response) => {
-  response.render('dashBoard', {name: request.user.firstName}); 
-});
-
-sports.get('/viewEvent', connectEnsureLogin.ensureLoggedIn(), async(request, response) => {
-
-  try{
-    const eventId=request.query.eventId;
-  console.log(eventId);
-    const eventCont = await AllEvents.findOne({
-      where: {id:eventId}}
-      );
-      let flag=0;
-      var enddate = new Date(eventCont.eventEndDate);
-      if(enddate<new Date()){
-        flag=1
-      }
-      console.log(eventCont.eventEndDate+"      "+new Date(),+"          "+flag)
-      console.log(request.user.email)
-    if(request.user.email==="balu1234@gmail.com"){
-        response.render("viewEvent",{eventCont, eventId,flag,hex})
-    }
-
-    else{
-      response.render("SviewEvent",{eventCont, eventId,flag,hex})
-    }
-  }
-  catch(err){
-    console.log(err);
-}
-});
-
-
-
-sports.get("/myEvents", connectEnsureLogin.ensureLoggedIn(), async (request, response) => {
+async function deleteEventHandler(req, res) {
+  const eventId = req.query.eventId;
+  console.log("ggdghfghfhfhF" + eventId);
   try {
-      
-      const currentUserId = request.user.id;
-      const data = await Registers.findAll({
-          attributes: ['id', 'userId', 'eventId'],
-          where: {
-              userId: currentUserId
-          }
-      });
-
-      const eventIdArray = data.map(entry => entry.eventId);
-
-      const formattedData = [];
-
-      for (let i = 0; i < eventIdArray.length; i++) {
-          const eventDetails = await AllEvents.findOne({
-              where: {
-                  id: eventIdArray[i]
-              }
-          });
-          formattedData.push(eventDetails);
+    await AllEvents.destroy({
+      where: {
+        id: eventId
       }
-
-      hex=1;
-      response.render('myEvents', { formattedData ,name:request.user.firstName});
-  } catch (error) {
-      console.error("Error retrieving data:", error);
-      response.status(500).send("Internal Server Error");
-  }
-});
-
-
-sports.get("/viewRegisters",connectEnsureLogin.ensureLoggedIn(),async(request, response)=>{
-
-  try {
-
-    const eventId = request.query.eventId;
-    const data = await Registers.findAll({
-        attributes: ['id', 'userId', 'eventId'],
     });
-    const temp=[];
-    for(let i=0 ; i<data.length;i++){
-        if(data[i].eventId==eventId){
-          temp.push(data[i])
-        }
-    }
-
-    const userIdArray = temp.map(entry => entry.userId);
-
-    const formattedData = [];
-
-    for (let i = 0; i < userIdArray.length; i++) {
-        const userDetails = await User.findOne({
-            where: {
-                id: userIdArray[i]
-            }
-        });
-        formattedData.push(userDetails);
-    }
-
-    
-    response.render('viewRegisters', { formattedData ,name:request.user.firstName});
-} catch (error) {
-    console.error("Error retrieving data:", error);
-    response.status(500).send("Internal Server Error");
-}
-})
-
-sports.get("/registerEvent",connectEnsureLogin.ensureLoggedIn(),async(request, response)=>{
-  const ev=request.query.eventId;
-  try{
-    await Registers.create({
-      userId:request.user.id,
-      eventId:ev
-
-    })
-    response.redirect("/myEvents");
+    res.redirect('/allEvents');
   }
-  catch(err){
-    console.log(err)
-  } 
-})
-
-sports.get("/unRegister",connectEnsureLogin.ensureLoggedIn(),async(request, response)=>{
-  const ev=request.query.eventId;
-  try{
-    await Registers.destroy({
-      where :{
-        userId:request.user.id,
-        eventId:ev
-      }
-    })
-    response.redirect("/myEvents");
-  }
-  catch(err){
+  catch (err) {
     console.log(err);
   }
-})
+}
 
+function dashBoardHandler(req, res) {
+  res.render('dashBoard', { name: req.user.firstName });
+}
 
+async function viewEventHandler(req, res) {
+  try {
+    const eventId = req.query.eventId;
+    console.log(eventId);
+    const eventCont = await AllEvents.findOne({ where: { id: eventId } });
+    let flag = 0;
+    var enddate = new Date(eventCont.eventEndDate);
+    if (enddate < new Date()) {
+      flag = 1;
+    }
+    console.log(eventCont.eventEndDate + "      " + new Date() + "          " + flag);
+    console.log(req.user.email);
+    if (req.user.email === "balu1234@gmail.com") {
+      res.render("viewEvent", { eventCont, eventId, flag, hex });
+    } else {
+      res.render("SviewEvent", { eventCont, eventId, flag, hex });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
 
+async function myEventsHandler(req, res) {
+  try {
+    const currentUserId = req.user.id;
+    const data = await Registers.findAll({
+      attributes: ['id', 'userId', 'eventId'],
+      where: { userId: currentUserId }
+    });
+    const eventIdArray = data.map(entry => entry.eventId);
+    const formattedData = [];
+    for (let i = 0; i < eventIdArray.length; i++) {
+      const eventDetails = await AllEvents.findOne({
+        where: { id: eventIdArray[i] }
+      });
+      formattedData.push(eventDetails);
+    }
+    hex = 1;
+    res.render('myEvents', { formattedData, name: req.user.firstName });
+  } catch (error) {
+    console.error("Error retrieving data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+}
 
-module.exports=sports;
+async function viewRegistersHandler(req, res) {
+  try {
+    const eventId = req.query.eventId;
+    const data = await Registers.findAll({ attributes: ['id', 'userId', 'eventId'] });
+    const temp = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].eventId == eventId) {
+        temp.push(data[i]);
+      }
+    }
+    const userIdArray = temp.map(entry => entry.userId);
+    const formattedData = [];
+    for (let i = 0; i < userIdArray.length; i++) {
+      const userDetails = await User.findOne({
+        where: { id: userIdArray[i] }
+      });
+      formattedData.push(userDetails);
+    }
+    res.render('viewRegisters', { formattedData, name: req.user.firstName });
+  } catch (error) {
+    console.error("Error retrieving data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+async function registerEventHandler(req, res) {
+  const ev = req.query.eventId;
+  try {
+    await Registers.create({ userId: req.user.id, eventId: ev });
+    res.redirect("/myEvents");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function unRegisterHandler(req, res) {
+  const ev = req.query.eventId;
+  try {
+    await Registers.destroy({ where: { userId: req.user.id, eventId: ev } });
+    res.redirect("/myEvents");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = sports;
